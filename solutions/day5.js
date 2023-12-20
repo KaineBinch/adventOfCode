d55771a82a902b9c05ed4b1f85b013674c950551
KaineBinch <kainebinch@live.com>
2023-12-20 03:02:46 +0000

    G-wip: day5 part2 answer too high, target incorrect

--- solutions/day5.js
+++ solutions/day5.js
@@ -35,6 +35,8 @@ const getSeedLocation = (seed, maps) => {
   return curr
 }
 
+// TODO: INFINITY? WHY? JACSON?
+
 const solvePartOne = (seeds, maps) => {
   let min = Infinity
   for (const seed of seeds) {
@@ -43,10 +45,125 @@ const solvePartOne = (seeds, maps) => {
   return min
 }
 
+const parseSeedRange = (seeds) => {
+  const newSeeds = []
+  for (let i = 0; i < seeds.length - 1; i += 2) {
+    newSeeds.push({
+      start: seeds[i],
+      length: seeds[i + 1]
+    })
+  }
+  return newSeeds
+}
+
+const getMaxTarget = (map) => {
+  let maxTarget = -1
+  for (const { dest, length } of map) {
+    maxTarget = Math.max(maxTarget, dest + length)
+  }
+  return maxTarget
+}
+
+const findSeedRangeOffset = (seedRange, target, maxTarget) => {
+  if (maxTarget >= seedRange.start && target < seedRange.start + seedRange.length) {
+    return target < seedRange.start ? seedRange.start - target : 0
+  }
+  return -1
+}
+
+const getRangesForTargetRange = (target, maxTarget, map) => {
+  const ranges = []
+  const min = { source: Infinity, dest: Infinity }
+  const max = { source: -1, dest: -1 }
+  for (const { source, dest, length } of map) {
+    if (dest < min.dest) min.dest = dest
+    if (source < min.source) min.source = source
+    if (dest > max.dest) max.dest = dest
+    if (source > max.source) max.source = source
+    ranges.push({
+      sourceStart: source,
+      sourceEnd: source + length,
+      destStart: dest,
+      destEnd: dest + length
+    })
+  }
+  if (min.source - 1 > target) ranges.push({
+    sourceStart: target,
+    sourceEnd: min.source - 1,
+    destStart: maxTarget,
+    destEnd: min.source - 1
+  })
+  if (max.source + 1 < maxTarget) ranges.push({
+    sourceStart: max.source + 1,
+    sourceEnd: maxTarget,
+    destStart: max.source + 1,
+    destEnd: maxTarget
+  })
+  return ranges
+}
+
+
+const getMinMapInputRangesForTargetRange = (target, maxTarget, map) => {
+  const resultRanges = []
+  const possibleRanges = getRangesForTargetRange(target, maxTarget, map)
+  for (const range of possibleRanges) {
+    const { sourceStart, sourceEnd, destStart, destEnd } = range
+    if (target + maxTarget >= destStart && target < destEnd) {
+      let startDiff = target - destStart
+      if (startDiff < 0) startDiff = 0
+      let endDiff = maxTarget - destEnd
+      if (endDiff < 0) endDiff = 0
+      if (sourceStart + startDiff > sourceEnd - endDiff) continue
+      resultRanges.push({
+        sourceStart: sourceStart + startDiff,
+        sourceEnd: sourceEnd - endDiff,
+        destStart: destStart + startDiff
+      })
+    }
+  }
+  resultRanges.sort((a, b) => a.sourceEnd - b.sourceEnd)
+  return resultRanges
+}
+
+const recursivelyGetLowestTarget = (seedRanges, maps, target, maxTarget, mapIndex) => {
+  if (mapIndex < 0) {
+    for (const seedRange of seedRanges) {
+      const offset = findSeedRangeOffset(seedRange, target, maxTarget)
+      if (offset != -1) {
+        console.log(target, offset)
+        return target + offset
+      }
+    }
+    return -1
+  }
+
+  const map = maps[mapIndex]
+  const ranges = getMinMapInputRangesForTargetRange(target, maxTarget, map)
+  for (const { sourceStart, sourceEnd, destStart } of ranges) {
+    const result = recursivelyGetLowestTarget(seedRanges, maps, sourceStart, sourceEnd, mapIndex - 1)
+    if (result != -1) {
+      const diff = destStart - sourceStart
+      return result + diff
+    }
+  }
+  return -1
+}
+
+const solvePartTwo = (seedRanges, maps) => {
+  const target = 0
+  const maxTarget = getMaxTarget(maps[maps.length - 1])
+  return recursivelyGetLowestTarget(seedRanges, maps, target, maxTarget, maps.length - 1)
+
+}
+
+
+
 const solution = (input) => {
   const [seeds, maps] = parseInput(input)
   const partOne = solvePartOne(seeds, maps)
+  const seedRanges = parseSeedRange(seeds)
+  const partTwo = solvePartTwo(seedRanges, maps)
 
-  return { partOne }
+  return { partOne, partTwo }
 }
 module.exports = { solution }
